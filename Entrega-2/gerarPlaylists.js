183a1c939069578b439a3bd38c084e1e22e2ee80
Thaynan Nunes <thaynan.nunes@ccc.ufcg.edu.br>
2018-07-10 12:03:45 -0300

    Adicionado filtro para músicas pouco populares

--- Entrega-2/gerarPlaylists.js
+++ Entrega-2/gerarPlaylists.js
@@ -13,7 +13,7 @@ function kruskal(nodes, edges) {
     let forest = _.map(nodes, (node) => [node]);
     const sortedEdges = _.sortBy(edges, (edge) => edge[WEIGHTED_EDGE_INDEX]);
     
-    while(forest.length > 1) {
+    while(forest.length > 1 && sortedEdges.length > 0) {
         const edge = sortedEdges.pop();
 
         const n1 = edge[FIRST_NODE_INDEX],
@@ -51,38 +51,78 @@ function loadPlaylists() {
 };
 
 function createMusicsGraph() {
+    console.log("Criando grafo..");
     let edges = [];
     let nodes = [];
 
-    playlists.forEach((playlist, i) => {
+    playlists.forEach((playlist, h) => {
         
-        if(i < 2) {
-            playlist.tracks.forEach((track, index) => {
-                if(_.findIndex(nodes, (node) => node === track.track_name) === -1) {
-                    nodes.push(track.track_name);
-                }
-                playlist.tracks.forEach((otherTrack, otherIndex) => {
-                    if(index !== otherIndex) {
-                        const newIndex = _.findIndex(edges, (branch) => 
-                            (branch[FIRST_NODE_INDEX] === track.track_name && branch[SECOND_NODE_INDEX] === otherTrack.track_name)
-                            || (branch[SECOND_NODE_INDEX] === track.track_name && branch[FIRST_NODE_INDEX] === otherTrack.track_name));
+        if(h<1000) {
 
-                        if(newIndex !== -1) {
-                            edges[newIndex][WEIGHTED_EDGE_INDEX] = edges[newIndex][WEIGHTED_EDGE_INDEX] + 1;
-                        } else {
-                            edges.push([track.track_name, otherTrack.track_name, 1]);
-                        }
-                    }
-                });    
-            });
+        for(let i = 0; i < playlist.tracks.length; i++) {
+            const track = playlist.tracks[i];
+
+            if(_.findIndex(nodes, (node) => node === track.track_name) === -1) {
+                nodes.push(track.track_name);
+            }
+
+            for(let j = i+1; j < playlist.tracks.length; j++) {
+                const otherTrack = playlist.tracks[j];
+                const newIndex = _.findIndex(edges, (branch) => 
+                    (branch[FIRST_NODE_INDEX] === track.track_name && branch[SECOND_NODE_INDEX] === otherTrack.track_name)
+                    || (branch[SECOND_NODE_INDEX] === track.track_name && branch[FIRST_NODE_INDEX] === otherTrack.track_name));
+
+                if(newIndex !== -1) {
+                    edges[newIndex][WEIGHTED_EDGE_INDEX] = edges[newIndex][WEIGHTED_EDGE_INDEX] + 1;
+                } else {
+                    edges.push([track.track_name, otherTrack.track_name, 1]);
+                }
+            }
+        }
         }
     });
+    console.log("Grafo criado!");
     return {edges: edges, nodes: nodes};
 }
 
-//breakTree(kruskal(nodes, edges));
+function filterLittlePopularMusics() {
+    console.log("Filtrando-se as músicas pouco escutadas..");
+    let novaPlaylist = playlists.slice();
+    playlists.forEach((playlist, i) => {
+        
+        playlist.tracks.forEach((track, j) => {
+            
+            let contador = 0;
+            const trackName = track.track_name;
+            playlists.forEach((playlist2) => {
+                playlist2.tracks.forEach((track2) => {
+                    if(track2.track_name === trackName) {
+                        contador++;
+                    }
+                });
+            });
+            if(contador < 300) {
+                novaPlaylist[i].tracks.splice(j, 1);
+            }
+        });
+    });
+    console.log("Feito..");
+    return novaPlaylist.slice();
+}
 
-loadPlaylists();
-const graph = createMusicsGraph();
-console.log(graph.edges);
-console.log(kruskal(graph.nodes, graph.edges));
+(() => {
+    loadPlaylists();
+    playlists = filterLittlePopularMusics();
+
+    const graph = createMusicsGraph();
+
+    console.log("Filtrando-se músicas com poucas relações");
+    graph.edges = graph.edges.filter((edge) => edge[WEIGHTED_EDGE_INDEX] > 10);
+    console.log("Filtrou-se as arestas..");
+
+    console.log("Gerando árvore máxima geradora..");
+    const maximumSpanningTree = kruskal(graph.nodes, graph.edges);
+
+    console.log(maximumSpanningTree);
+    //breakTree(kruskal(nodes, edges));
+})();
